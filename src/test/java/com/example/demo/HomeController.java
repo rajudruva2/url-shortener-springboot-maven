eabf3537e5b05f585e695f5c29f8dc38e86a557b
Scaffolder <scaffolder@backstage.io>
2026-06-04 22:13:46 +0000

    initial commit

---
+++ src/test/java/com/example/demo/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Raj Platform Spring Boot Template";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "UP";
+    }
+}
